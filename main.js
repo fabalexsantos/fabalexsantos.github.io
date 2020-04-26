729bca3106b54e3ac60ab921ebd3f909980e3de6
fabalexsantos <fabalexsantos@gmail.com>
2020-04-26 00:22:03 -0300

    external JS file

---
+++ main.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var string = document.lastModified;
+document.getElementById("lastUpdated").textContent = `Last Updated: ${string}`
+document.getElementById("year").textContent = d.getFullYear();
