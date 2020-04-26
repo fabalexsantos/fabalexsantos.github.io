97e1eea9a8679b65bab775dcb1827088631a11d1
fabalexsantos <fabalexsantos@gmail.com>
2020-04-26 00:30:38 -0300

    teste

--- images/js/main.js
+++ images/js/main.js
@@ -2,3 +2,4 @@ var d = new Date();
 var string = document.lastModified;
 document.getElementById("lastUpdated").textContent = `Last Updated: ${string}`
 document.getElementById("year").textContent = d.getFullYear();
+console.log("teste")
