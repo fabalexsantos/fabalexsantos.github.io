8b503c5350e9791255dfa40cb43ff5868e598dad
fabalexsantos <fabalexsantos@gmail.com>
2020-07-03 09:38:06 -0300

    week 11

---
+++ lesson11/js/cities.js
@@ -0,0 +1,5 @@
+if (dayName == 'Friday') {
+  document.getElementById('message').classList.add('showme');
+} else if (dayName) {
+  document.getElementById('message').classList.add('hide');
+}
---
+++ lesson11/js/weather.js
@@ -0,0 +1,17 @@
+//ADD the key and change units to imperial
+const apiURL =
+  '//api.openweathermap.org/data/2.5/weather?id=5604473&appid=79c66cddcbc0ca13f6645125edf877d6&units=imperial';
+
+//Go fetch it and then wait for a response.
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    //Once it comes back, display it to the console.
+    console.log(weatherInfo);
+
+    document.getElementById('temp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('speed').innerHTML = weatherInfo.wind.speed;
+    document.getElementById('condition').innerHTML =
+      weatherInfo.weather[0].description;
+    document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
+  }); //end of "then" fat arrow function
